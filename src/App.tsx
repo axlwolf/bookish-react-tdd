c19f5b45179df9ef77d7f90273937581d643b1ea
GDL-i5143 <user@GDL-i5143s-MacBook-Pro.local>
2023-11-16 10:51:15 -0600

    3. BookList component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import { Typography } from "@mui/material";
+import type { Book } from "./types/types";
+import BookList from "./components/BookList";
 
 export const App = () => {
+	const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+
 	return (
-		<Typography variant="h2" component="h2" data-test="heading">
-			Bookish
-		</Typography>
+		<>
+			<Typography variant="h2" component="h2" data-test="heading">
+				Bookish
+			</Typography>
+			<div data-test="book-list">
+				{/* <div className="book-item">
+					<h2>Refactoring</h2>
+				</div>
+				<div className="book-item">
+					<h2>Domain-driven design</h2>
+				</div> */}
+				{/* {books.map((book) => (
+					<div className="book-item">
+						<h2 className="title">{book.name}</h2>
+					</div>
+				))} */}
+				<BookList books={books} />
+			</div>
+		</>
 	);
 };
